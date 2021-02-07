91004ac666a0932c1261e879eaee25b955145836
Rodrigo Augusto <rodrigoaugusto839@gmail.com>
2021-02-07 17:50:12 -0300

    CategoriaDAO- metodo alterar Dados

--- src/testes/TesteCategoria.java
+++ src/testes/TesteCategoria.java
@@ -8,9 +8,12 @@ import persistencia.CategoriaDAO;
 public class TesteCategoria {
     public static void main(String[] args) {
         Categoria categoria=new Categoria();
-        categoria.setNome("Ação");
+        categoria.setNome("Comedia");
         categoria.setTipo('F'); //F-filme J-Jogo
         
-        CategoriaDAO.inserir(categoria);
+        //CategoriaDAO.inserir(categoria);
+        
+        categoria.setId(1);
+        CategoriaDAO.alterar(categoria);
     }
 }
