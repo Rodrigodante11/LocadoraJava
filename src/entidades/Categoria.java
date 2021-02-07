d986fd17b95f7a283e515315481bedefa474c0d6
Rodrigo Augusto <rodrigoaugusto839@gmail.com>
2021-02-07 09:55:30 -0300

    LLocadora Java

---
+++ src/entidades/Categoria.java
@@ -0,0 +1,33 @@
+//Ctrl+alt+shift+E
+package entidades;
+
+
+public class Categoria {
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public char getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(char tipo) {
+        this.tipo = tipo;
+    }
+    private int id;
+    private String nome;
+    private char tipo;
+}
