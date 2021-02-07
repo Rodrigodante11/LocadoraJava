2624fadebf0ec98d74130b507621860ccca9d6cf
Rodrigo Augusto <rodrigoaugusto839@gmail.com>
2021-02-07 10:28:34 -0300

    Criacao de entidades do Projeto

---
+++ src/entidades/Cliente.java
@@ -0,0 +1,44 @@
+
+package entidades;
+
+
+public class Cliente {
+    private int Id;
+    private String nome;
+    private String email;
+    private String telefone;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    
+}
---
+++ src/entidades/ItemLocacao.java
@@ -0,0 +1,42 @@
+
+package entidades;
+
+
+public class ItemLocacao {
+    private int id;
+    private Double valor;
+    private Filme filme;
+    private Jogo jogo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Filme getFilme() {
+        return filme;
+    }
+
+    public void setFilme(Filme filme) {
+        this.filme = filme;
+    }
+
+    public Jogo getJogo() {
+        return jogo;
+    }
+
+    public void setJogo(Jogo jogo) {
+        this.jogo = jogo;
+    }
+}
